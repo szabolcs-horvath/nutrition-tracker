884bf1664029dddf6450dbeab3bcaa40658afb76
Szabolcs Horvath <shorvath@cloudera.com>
2024-10-18 21:04:41 +0200

    Serve a simple endpoint

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+)
 
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	_, err := w.Write([]byte("{\"test\":\"value\"}"))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", handleRoot)
+	log.Fatal(http.ListenAndServe(":80", nil))
 }
