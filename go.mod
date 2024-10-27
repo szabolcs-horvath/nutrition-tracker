94a3550712cf2272dc57ed695b056d7017435924
Szabolcs Horvath <shorvath@cloudera.com>
2024-10-27 21:01:12 +0100

    Added `godotenv`

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module shorvath/nutrition-tracker
 go 1.23.1
 
 require github.com/mattn/go-sqlite3 v1.14.24
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"log/slog"
 	"net/http"
+	"os"
 	"shorvath/nutrition-tracker/http_server"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		slog.Error("Failed to load .env file!")
+		panic(1)
+	}
+	port := os.Getenv("PORT")
+
 	for k, v := range http_server.GetRoutes() {
 		http.HandleFunc(k, v)
 	}
-	log.Fatalln(http.ListenAndServe(":80", nil))
+
+	slog.Error("Exiting: %v", http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
