5fde2b2ed44258f678b400c6661e1f916670bec0
Szabolcs Horvath <shorvath@cloudera.com>
2024-11-30 15:04:34 +0100

    Added missing CRUD endpoints for nutritions

--- http_server/routes/api/v1/nutritions/routes.go
+++ http_server/routes/api/v1/nutritions/routes.go
@@ -1,6 +1,7 @@
 package nutritions
 
 import (
+	"encoding/json"
 	"net/http"
 	"shorvath/nutrition-tracker/helpers"
 	"shorvath/nutrition-tracker/repository"
@@ -11,8 +12,11 @@ const Prefix = "/nutritions"
 
 func Handlers() map[string]http.HandlerFunc {
 	return map[string]http.HandlerFunc{
-		"GET /":     listHandler,
-		"GET /{id}": findByIdHandler,
+		"GET /":        listHandler,
+		"GET /{id}":    findByIdHandler,
+		"POST /":       createHandler,
+		"PUT /":        updateHandler,
+		"DELETE /{id}": deleteHandler,
 	}
 }
 
@@ -43,6 +47,57 @@ func findByIdHandler(w http.ResponseWriter, r *http.Request) {
 		if err = helpers.WriteJson(w, http.StatusOK, nutrition); err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 		}
+	} else {
+		http.Error(w, "You need to specify the id of the item!", http.StatusBadRequest)
+	}
+}
+
+func createHandler(w http.ResponseWriter, r *http.Request) {
+	var requestNutrition *repository.Nutrition
+	if err := json.NewDecoder(r.Body).Decode(requestNutrition); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	nutrition, err := repository.CreateNutrition(r.Context(), requestNutrition)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if err = helpers.WriteJson(w, http.StatusCreated, nutrition); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+}
+
+func updateHandler(w http.ResponseWriter, r *http.Request) {
+	var requestNutrition *repository.Nutrition
+	if err := json.NewDecoder(r.Body).Decode(requestNutrition); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	nutrition, err := repository.UpdateNutrition(r.Context(), requestNutrition)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if err = helpers.WriteJson(w, http.StatusOK, nutrition); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+}
+
+func deleteHandler(w http.ResponseWriter, r *http.Request) {
+	idParam := r.PathValue("id")
+	if idParam != "" {
+		id, err := strconv.ParseInt(idParam, 10, 64)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		err = repository.DeleteNutrition(r.Context(), id)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusNoContent)
 	} else {
 		http.Error(w, "You need to specify the id of the item!", http.StatusBadRequest)
 	}
